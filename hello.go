8d4637e545c8e0e323b8a9a8fd8a2eb7cc2ffd02
LordNykkon <mjasinksi12000@gmail.com>
2022-01-04 00:59:09 -0500

    Initial commit

---
+++ hello.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+
+	"rsc.io/quote"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+	quotes := map[int]func() string{
+		0: quote.Glass,
+		1: quote.Go,
+		2: quote.Hello,
+		3: quote.Opt,
+	}
+	rand.Seed(time.Now().UnixNano())
+	fmt.Println(quotes[rand.Intn(4)]())
+}
